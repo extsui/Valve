5e37d712245bcbd1c59a4152d36cff572cab5143
extsui <en422641@s.okayama-u.ac.jp>
2022-03-26 00:59:35 +0900

    (ValveDemo) 全ての Encoder をグラフに連動

--- Tools/ValveDemo/ValveDemo/ViewModel.cs
+++ Tools/ValveDemo/ValveDemo/ViewModel.cs
@@ -34,41 +34,59 @@ namespace ValveDemo
         public PlotModel Model { get; } = new PlotModel();
         public PlotController Controller { get; } = new PlotController();
 
-        // 軸の設定
-        public OxyPlot.Axes.LinearAxis AxisX { get; } = new OxyPlot.Axes.LinearAxis();
-        public OxyPlot.Axes.LinearAxis AxisY { get; } = new OxyPlot.Axes.LinearAxis();
-
         // データを保存するコレクション
-        public ObservableCollection<DataPoint> Datas { get; private set; } = new ObservableCollection<DataPoint>();
+        public ObservableCollection<DataPoint> EncoderDatas1 { get; private set; } = new ObservableCollection<DataPoint>();
+        public ObservableCollection<DataPoint> EncoderDatas2 { get; private set; } = new ObservableCollection<DataPoint>();
+        public ObservableCollection<DataPoint> EncoderDatas3 { get; private set; } = new ObservableCollection<DataPoint>();
+        public ObservableCollection<DataPoint> EncoderDatas4 { get; private set; } = new ObservableCollection<DataPoint>();
 
         // グラフの設定
         public void InitGraph()
         {
+            var axisX = new OxyPlot.Axes.LinearAxis();
+            var axisY = new OxyPlot.Axes.LinearAxis();
+
             // X軸の設定
-            AxisX.Position = OxyPlot.Axes.AxisPosition.Bottom;    // 軸の位置(topにしたら、目盛りが上にくる)
-            AxisX.Minimum = 0;
-            AxisX.Maximum = 100;
+            axisX.Position = OxyPlot.Axes.AxisPosition.Bottom;    // 軸の位置(topにしたら、目盛りが上にくる)
+            axisX.Minimum = 0;
+            axisX.Maximum = 100;
 
             // Y軸の設定
-            AxisY.Position = OxyPlot.Axes.AxisPosition.Left;      // Y軸の位置(Rightにしたら、目盛りが右にくる)
-            AxisY.Minimum = sbyte.MinValue;
-            AxisY.Maximum = sbyte.MaxValue;
+            axisY.Position = OxyPlot.Axes.AxisPosition.Left;      // Y軸の位置(Rightにしたら、目盛りが右にくる)
+            axisY.Minimum = sbyte.MinValue;
+            axisY.Maximum = sbyte.MaxValue;
 
             // 設定した軸をモデルにセット
-            Model.Axes.Add(AxisX);
-            Model.Axes.Add(AxisY);
+            Model.Axes.Add(axisX);
+            Model.Axes.Add(axisY);
 
             // 線グラフ
-            var LineSeries = new OxyPlot.Series.LineSeries();
-            LineSeries.Title = "Line";
-            LineSeries.Color = OxyColor.FromArgb(0xff, 0xff, 0, 0);     // 上の線の色
-            LineSeries.StrokeThickness = 2;                             // 線の太さ
-            // データを関連付け
-            LineSeries.ItemsSource = Datas;
+            var colors = new List<OxyColor>()
+            {
+                OxyColor.FromArgb(0xFF, 246, 173, 60 ), // 橙
+                OxyColor.FromArgb(0xFF, 0,   169, 95 ), // 緑
+                OxyColor.FromArgb(0xFF, 232, 82,  152), // 桃
+                OxyColor.FromArgb(0xFF, 24,  127, 196), // 青
+            };
+            var encoderDatas = new List<ObservableCollection<DataPoint>>()
+            {
+                EncoderDatas1,
+                EncoderDatas2,
+                EncoderDatas3,
+                EncoderDatas4,
+            };
+            // TODO: Tuple とかでまとめられるハズ
+            for (int i = 0; i < 4; i++)
+            {
+                var lineSeries = new OxyPlot.Series.LineSeries();
+                lineSeries.Title = "Line";
+                lineSeries.StrokeThickness = 5 *2;
+                lineSeries.Color = colors[i];
+                lineSeries.ItemsSource = encoderDatas[i];
 
-            // グラフをモデルに追加
-            Model.Series.Add(LineSeries);
-
+                // グラフをモデルに追加
+                Model.Series.Add(lineSeries);
+            }
             // セットした内容を反映させる
             Model.InvalidatePlot(true);
         }
@@ -144,20 +162,34 @@ namespace ValveDemo
             EncoderValue3.Value = (double)m_Valve.EncoderValue[2];
             EncoderValue4.Value = (double)m_Valve.EncoderValue[3];
 
-            // TODO: 要整理
             // グラフの更新
-            Datas.Clear();
-            var queue = m_ValveQueue.ReadAll().ToArray();
-            var points = new List<DataPoint>();
-            for (int i = 0; i < queue.Length; i++)
             {
-                points.Add(new DataPoint(i, queue[i].EncoderValue[0]));
+                var queue = m_ValveQueue.ReadAll().ToArray();
+
+                // TODO: 要整理
+                EncoderDatas1.Clear();
+                EncoderDatas2.Clear();
+                EncoderDatas3.Clear();
+                EncoderDatas4.Clear();
+
+                var points1 = new List<DataPoint>();
+                var points2 = new List<DataPoint>();
+                var points3 = new List<DataPoint>();
+                var points4 = new List<DataPoint>();
+                for (int i = 0; i < queue.Length; i++)
+                {
+                    points1.Add(new DataPoint(i, queue[i].EncoderValue[0]));
+                    points2.Add(new DataPoint(i, queue[i].EncoderValue[1]));
+                    points3.Add(new DataPoint(i, queue[i].EncoderValue[2]));
+                    points4.Add(new DataPoint(i, queue[i].EncoderValue[3]));
+                }
+                points1.ForEach(point => EncoderDatas1.Add(point));
+                points2.ForEach(point => EncoderDatas2.Add(point));
+                points3.ForEach(point => EncoderDatas3.Add(point));
+                points4.ForEach(point => EncoderDatas4.Add(point));
+
+                Model.InvalidatePlot(true);
             }
-            foreach (var point in points)
-            {
-                Datas.Add(point);
-            }
-            Model.InvalidatePlot(true);
         }
 
         public void WriteLine(string value)
