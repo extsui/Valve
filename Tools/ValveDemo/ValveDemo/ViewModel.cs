100bcba377d67155f32bdcb7d0bf6c53dc81fd37
extsui <en422641@s.okayama-u.ac.jp>
2022-03-27 00:13:23 +0900

    (ValveDemo) グラフをリアルタイム描画に修正
    * X軸の時間範囲を追従
    * チェックボックスと各エンコーダの線グラフを連動

--- Tools/ValveDemo/ValveDemo/ViewModel.cs
+++ Tools/ValveDemo/ValveDemo/ViewModel.cs
@@ -1,8 +1,9 @@
 ﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO.Ports;
 using System.Windows;
@@ -21,72 +22,61 @@ namespace ValveDemo
         public ReactiveProperty<bool> ButtonConnect_IsEnabled { get; set; } = new ReactiveProperty<bool>(true);
         public ReactiveProperty<bool> ButtonDisconnect_IsEnabled { get; set; } = new ReactiveProperty<bool>(false);
 
+        // スライダーと連動
         public ReactiveProperty<double> EncoderValue1 { get; set; } = new ReactiveProperty<double>(0);
         public ReactiveProperty<double> EncoderValue2 { get; set; } = new ReactiveProperty<double>(0);
         public ReactiveProperty<double> EncoderValue3 { get; set; } = new ReactiveProperty<double>(0);
         public ReactiveProperty<double> EncoderValue4 { get; set; } = new ReactiveProperty<double>(0);
 
+        // グラフの描画と連動
+        public ReactiveProperty<bool> EncoderEnabled1 { get; set; } = new ReactiveProperty<bool>(true);
+        public ReactiveProperty<bool> EncoderEnabled2 { get; set; } = new ReactiveProperty<bool>(true);
+        public ReactiveProperty<bool> EncoderEnabled3 { get; set; } = new ReactiveProperty<bool>(true);
+        public ReactiveProperty<bool> EncoderEnabled4 { get; set; } = new ReactiveProperty<bool>(true);
+
         private SerialPortManager m_SerialPortManager;
         private Valve m_Valve;
         private ValveQueue m_ValveQueue;
 
-        // OxyPlotのためのモデルとコントローラー
-        public PlotModel Model { get; } = new PlotModel();
-        public PlotController Controller { get; } = new PlotController();
-
-        // データを保存するコレクション
-        public ObservableCollection<DataPoint> EncoderDatas1 { get; private set; } = new ObservableCollection<DataPoint>();
-        public ObservableCollection<DataPoint> EncoderDatas2 { get; private set; } = new ObservableCollection<DataPoint>();
-        public ObservableCollection<DataPoint> EncoderDatas3 { get; private set; } = new ObservableCollection<DataPoint>();
-        public ObservableCollection<DataPoint> EncoderDatas4 { get; private set; } = new ObservableCollection<DataPoint>();
+        // OxyPlot 用
+        public PlotController Controller { get; private set; } = new PlotController();
+        private PlotModel _graphModel;
+        public PlotModel GraphModel
+        {
+            get => _graphModel;
+            set
+            {
+                _graphModel = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+            }
+        }
 
         // グラフの設定
         public void InitGraph()
         {
-            var axisX = new OxyPlot.Axes.LinearAxis();
-            var axisY = new OxyPlot.Axes.LinearAxis();
+            var plotModel = new PlotModel();
 
-            // X軸の設定
-            axisX.Position = OxyPlot.Axes.AxisPosition.Bottom;    // 軸の位置(topにしたら、目盛りが上にくる)
-
-            // Y軸の設定
-            axisY.Position = OxyPlot.Axes.AxisPosition.Left;      // Y軸の位置(Rightにしたら、目盛りが右にくる)
-            axisY.Minimum = sbyte.MinValue;
-            axisY.Maximum = sbyte.MaxValue;
-
-            // 設定した軸をモデルにセット
-            Model.Axes.Add(axisX);
-            Model.Axes.Add(axisY);
-
-            // 線グラフ
-            var colors = new List<OxyColor>()
+            // 軸の設定
             {
-                OxyColor.FromArgb(0xFF, 246, 173, 60 ), // 橙
-                OxyColor.FromArgb(0xFF, 0,   169, 95 ), // 緑
-                OxyColor.FromArgb(0xFF, 232, 82,  152), // 桃
-                OxyColor.FromArgb(0xFF, 24,  127, 196), // 青
-            };
-            var encoderDatas = new List<ObservableCollection<DataPoint>>()
-            {
-                EncoderDatas1,
-                EncoderDatas2,
-                EncoderDatas3,
-                EncoderDatas4,
-            };
-            // TODO: Tuple とかでまとめられるハズ
-            for (int i = 0; i < 4; i++)
-            {
-                var lineSeries = new OxyPlot.Series.LineSeries();
-                lineSeries.Title = "Line";
-                lineSeries.StrokeThickness = 5 *2;
-                lineSeries.Color = colors[i];
-                lineSeries.ItemsSource = encoderDatas[i];
+                var axisX = new LinearAxis();
+                var axisY = new LinearAxis();
 
-                // グラフをモデルに追加
-                Model.Series.Add(lineSeries);
+                // X軸の設定
+                axisX.Position = AxisPosition.Bottom;    // 軸の位置(topにしたら、目盛りが上にくる)
+                axisX.Minimum = 0;
+                axisX.Maximum = 5;
+
+                // Y軸の設定
+                axisY.Position = AxisPosition.Left;      // Y軸の位置(Rightにしたら、目盛りが右にくる)
+                axisY.Minimum = sbyte.MinValue;
+                axisY.Maximum = sbyte.MaxValue;
+
+                // 設定した軸をモデルにセット
+                plotModel.Axes.Add(axisX);
+                plotModel.Axes.Add(axisY);
             }
-            // セットした内容を反映させる
-            Model.InvalidatePlot(true);
+
+            GraphModel = plotModel;
         }
 
         public ViewModel()
@@ -162,35 +152,88 @@ namespace ValveDemo
 
             // グラフの更新
             {
+                var plotModel = new PlotModel();
                 var queue = m_ValveQueue.ReadAll().ToArray();
 
-                // TODO: 要整理
-                EncoderDatas1.Clear();
-                EncoderDatas2.Clear();
-                EncoderDatas3.Clear();
-                EncoderDatas4.Clear();
-
-                var points1 = new List<DataPoint>();
-                var points2 = new List<DataPoint>();
-                var points3 = new List<DataPoint>();
-                var points4 = new List<DataPoint>();
-
-                // タイムスタンプ (ms) を元に秒単位の時間軸にする
-                foreach (var item in queue)
+                // 線グラフの設定
                 {
-                    var x = (double)item.TimeStamp / 1000;
-                    points1.Add(new DataPoint(x, item.EncoderValue[0]));
-                    points2.Add(new DataPoint(x, item.EncoderValue[1]));
-                    points3.Add(new DataPoint(x, item.EncoderValue[2]));
-                    points4.Add(new DataPoint(x, item.EncoderValue[3]));
+                    var points1 = new List<DataPoint>();
+                    var points2 = new List<DataPoint>();
+                    var points3 = new List<DataPoint>();
+                    var points4 = new List<DataPoint>();
+
+                    // タイムスタンプ (ms) を元に秒単位の時間軸にする
+                    foreach (var item in queue)
+                    {
+                        var x = (double)item.TimeStamp / 1000;
+                        points1.Add(new DataPoint(x, item.EncoderValue[0]));
+                        points2.Add(new DataPoint(x, item.EncoderValue[1]));
+                        points3.Add(new DataPoint(x, item.EncoderValue[2]));
+                        points4.Add(new DataPoint(x, item.EncoderValue[3]));
+                    }
+
+                    var colors = new List<OxyColor>()
+                    {
+                        OxyColor.FromArgb(0xFF, 246, 173, 60 ), // 橙
+                        OxyColor.FromArgb(0xFF, 232, 82,  152), // 桃
+                        OxyColor.FromArgb(0xFF, 0,   169, 95 ), // 緑
+                        OxyColor.FromArgb(0xFF, 24,  127, 196), // 青
+                    };
+
+                    var encoderDatas = new List<List<DataPoint>>()
+                    {
+                        points1, points2, points3, points4,
+                    };
+
+                    var enabled = new List<bool>()
+                    {
+                        EncoderEnabled1.Value,
+                        EncoderEnabled2.Value,
+                        EncoderEnabled3.Value,
+                        EncoderEnabled4.Value,
+                    };
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (!enabled[i])
+                        {
+                            continue;
+                        }
+                        var lineSeries = new LineSeries();
+                        lineSeries.Title = $"#{i + 1}";
+                        lineSeries.StrokeThickness = 10;
+                        lineSeries.Color = colors[i];
+                        lineSeries.ItemsSource = encoderDatas[i];
+
+                        // グラフをモデルに追加
+                        plotModel.Series.Add(lineSeries);
+                    }
                 }
 
-                points1.ForEach(point => EncoderDatas1.Add(point));
-                points2.ForEach(point => EncoderDatas2.Add(point));
-                points3.ForEach(point => EncoderDatas3.Add(point));
-                points4.ForEach(point => EncoderDatas4.Add(point));
+                // 軸の設定
+                {
+                    var axisX = new LinearAxis();
+                    var axisY = new LinearAxis();
 
-                Model.InvalidatePlot(true);
+                    // X軸の設定
+                    axisX.Position = AxisPosition.Bottom;    // 軸の位置(topにしたら、目盛りが上にくる)
+                    axisX.Minimum = (double)queue[0].TimeStamp / 1000;
+                    axisX.Maximum = axisX.Minimum + 5.0;
+                    axisX.Title = "時刻 (単位: 秒)";
+
+                    // Y軸の設定
+                    axisY.Position = AxisPosition.Left;      // Y軸の位置(Rightにしたら、目盛りが右にくる)
+                    axisY.Minimum = sbyte.MinValue;
+                    axisY.Maximum = sbyte.MaxValue;
+                    axisY.Title = "エンコーダ値";
+
+                    // 設定した軸をモデルにセット
+                    plotModel.Axes.Add(axisX);
+                    plotModel.Axes.Add(axisY);
+                }
+
+                GraphModel = plotModel;
+                GraphModel.InvalidatePlot(true);
             }
         }
 
