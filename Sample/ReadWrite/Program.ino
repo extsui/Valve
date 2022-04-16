215ab0d80a128c0be5827020b51b5cddceab0181
extsui <en422641@s.okayama-u.ac.jp>
2022-04-16 20:38:37 +0900

    (Valve) I2C テスト用のデータ送信サンプルを作成

--- Sample/ReadWrite/Program.ino
+++ Sample/ReadWrite/Program.ino
@@ -12,6 +12,151 @@ static Valve g_Valve;
 
 }
 
+void I2cReadWrite(uint8_t deviceAddress, const uint8_t *pTxData, uint8_t txSize, uint8_t *pOutRxData, uint8_t rxSize, uint8_t *pOutRealRxSize)
+{
+    // 送信フェーズ
+    g_Wire.beginTransmission(deviceAddress);
+    for (int i = 0; i < txSize; i++) {
+        g_Wire.write(pTxData[i]);
+    }
+
+    // 受信無しの場合は終了
+    if (rxSize == 0) {
+        g_Wire.endTransmission(true);
+        return;
+    }
+    g_Wire.endTransmission(false);
+    
+    // 受信フェーズ
+    uint8_t realRxSize = g_Wire.requestFrom(deviceAddress, rxSize);
+    for (int i = 0; i < realRxSize; i++) {
+        pOutRxData[i] = g_Wire.read();
+    }
+    *pOutRealRxSize = realRxSize;
+}
+
+constexpr uint8_t DeviceAddress = 0x40;
+uint8_t g_TxData[8];
+uint8_t g_RxData[8];
+uint8_t g_RxSize = 0;
+
+void Setup()
+{
+    memset(g_TxData, 0xFF, sizeof(g_TxData));
+    memset(g_RxData, 0xFF, sizeof(g_RxData));
+    g_RxSize = 0;
+}
+
+void PrintResult()
+{
+    LOG("Result: rxSize = %d\n  [ ", g_RxSize);
+    for (int i = 0; i < g_RxSize; i++) {
+        LOG("0x%02x ", g_RxData[i]);
+    }
+    LOG("]\n");
+}
+
+// 想定ケース
+// - (S), Address+W, MemAddress, (R), Address+R, ReadData, (P)
+// - (S), Address+W, MemAddress, (R), Address+R, ReadData, ReadData, (P)
+// - (S), Address+W, MemAddress, (R), Address+R, ReadData, ReadData, ReadData, (P)
+// - (S), Address+W, MemAddress, (R), Address+R, ReadData, ReadData, ReadData, ReadData, (P)
+// - (S), Address+W, MemAddress, WriteData, (P)
+// - (S), Address+W, MemAddress, WriteData, WriteData, (P)
+// - (S), Address+W, MemAddress, WriteData, WriteData, WriteData, (P)
+// - (S), Address+W, MemAddress, WriteData, WriteData, WriteData, WriteData, (P)
+// ※(S) = Stop Condition, (R) = Restart Condition, (P) = Stop Condition
+void I2cTest()
+{
+    // TORIAEZU: デバイス側で受信表示をする影響で間隔をあけておく
+    constexpr int TransactionIntervalMilliSeconds = 100;
+
+    LOG("== I2cTest (Read Write) ==\n");
+
+    Setup();
+    g_TxData[0] = 0x12;
+    I2cReadWrite(DeviceAddress, g_TxData, 1, g_RxData, 1, &g_RxSize);
+    PrintResult();
+
+    delay(TransactionIntervalMilliSeconds);
+
+    Setup();
+    g_TxData[0] = 0x12;
+    I2cReadWrite(DeviceAddress, g_TxData, 1, g_RxData, 2, &g_RxSize);
+    PrintResult();
+
+    delay(TransactionIntervalMilliSeconds);
+
+    Setup();
+    g_TxData[0] = 0x12;
+    I2cReadWrite(DeviceAddress, g_TxData, 1, g_RxData, 3, &g_RxSize);
+    PrintResult();
+
+    delay(TransactionIntervalMilliSeconds);
+
+    Setup();
+    g_TxData[0] = 0x12;
+    I2cReadWrite(DeviceAddress, g_TxData, 1, g_RxData, 4, &g_RxSize);
+    PrintResult();
+
+    delay(TransactionIntervalMilliSeconds);
+
+    LOG("== I2cTest (Write Only) ==\n");
+
+    Setup();
+    g_TxData[0] = 0xAA;
+    g_TxData[1] = 0xBB;
+    I2cReadWrite(DeviceAddress, g_TxData, 1 + 1, nullptr, 0, nullptr);
+
+    delay(TransactionIntervalMilliSeconds);
+
+    Setup();
+    g_TxData[0] = 0xAA;
+    g_TxData[1] = 0xBB;
+    g_TxData[2] = 0xCC;
+    I2cReadWrite(DeviceAddress, g_TxData, 1 + 2, nullptr, 0, nullptr);
+
+    delay(TransactionIntervalMilliSeconds);
+
+    Setup();
+    g_TxData[0] = 0xAA;
+    g_TxData[1] = 0xBB;
+    g_TxData[2] = 0xCC;
+    g_TxData[3] = 0xDD;
+    I2cReadWrite(DeviceAddress, g_TxData, 1 + 3, nullptr, 0, nullptr);
+
+    delay(TransactionIntervalMilliSeconds);
+
+    Setup();
+    g_TxData[0] = 0xAA;
+    g_TxData[1] = 0xBB;
+    g_TxData[2] = 0xCC;
+    g_TxData[3] = 0xDD;
+    g_TxData[4] = 0xEE;
+    I2cReadWrite(DeviceAddress, g_TxData, 1 + 4, nullptr, 0, nullptr);
+
+    delay(TransactionIntervalMilliSeconds);
+
+    LOG("Done.\n");
+}
+
+// 2byte write --> 2byte read
+void debugI2c()
+{
+    uint8_t rxData[2] = { 0xFF, 0xFF };
+
+    g_Wire.beginTransmission(0x40);
+    g_Wire.write(0x12);
+    g_Wire.write(0x34);
+    g_Wire.write(0x56);
+    g_Wire.endTransmission(false);
+    g_Wire.requestFrom(0x40, 2);
+    rxData[0] = g_Wire.read();
+    rxData[1] = g_Wire.read();
+
+    LOG("rxData = [ 0x%02x, 0x%02x ]\n", rxData[0], rxData[1]);
+}
+
 void setup()
 {
     // XIAO の Serial は USB-CDC 用
@@ -22,6 +167,11 @@ void setup()
     LOG("setup done!\n");
 
     g_Wire.begin();
+
+    I2cTest();
+    //debugI2c();
+    return;
+
     int result = g_Valve.Initialize(g_Wire, Valve::I2cAddress);
     // TORIAEZU: 実際の機器がまだないのでエラーになるはず
     LOG("result = %d (expected: -1)\n", result);
